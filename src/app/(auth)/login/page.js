85d90aca808ca36a3d25a3086207a12ac54ab5c0
Vega Anggaresta <vegaangga98@gmail.com>
2025-06-11 14:24:44 +0700

    auth login

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -14,17 +14,14 @@ import {
   Flex,
 } from "@mantine/core";
 import { useRouter } from "next/navigation";
-import { saveToken, getToken } from "@/api/auth";
+import { login, getToken } from "@/api/auth";
 
 export default function LoginPage() {
   const router = useRouter();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
-const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-const appId = process.env.NEXT_PUBLIC_APP_ID;
-
-  // ⛔ Jika sudah login, langsung redirect ke /dashboard
   useEffect(() => {
     const token = getToken();
     if (token) {
@@ -34,33 +31,9 @@ const appId = process.env.NEXT_PUBLIC_APP_ID;
 
   const handleLogin = async (e) => {
     e.preventDefault();
-
+    setLoading(true);
     try {
-      const res = await fetch(`${apiUrl}/authMob`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          nippos: username,
-          password: password,
-          idAplikasi: appId,
-        }),
-      });
-
-      const contentType = res.headers.get("content-type");
-
-      if (!res.ok) throw new Error(`HTTP ${res.status}`);
-      if (!contentType?.includes("application/json")) {
-        const html = await res.text();
-        console.error("Response bukan JSON:", html);
-        throw new Error("Server mengirim data bukan JSON");
-      }
-
-      const data = await res.json();
-      if (!data.token) throw new Error("Token tidak ditemukan");
-
-      saveToken(data.token);
+      await login({ nippos: username, password });
       router.push("/dashboard");
     } catch (err) {
       alert("Login gagal: " + err.message);
@@ -81,7 +54,7 @@ const appId = process.env.NEXT_PUBLIC_APP_ID;
               placeholder="Masukkan email atau nippos"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
-              required
+              
               mb="sm"
             />
             <PasswordInput
@@ -89,7 +62,7 @@ const appId = process.env.NEXT_PUBLIC_APP_ID;
               placeholder="Masukkan password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              required
+              
               mb="sm"
             />
             <Group mt="md" justify="space-between">
@@ -98,7 +71,7 @@ const appId = process.env.NEXT_PUBLIC_APP_ID;
                 Lupa Password?
               </Anchor>
             </Group>
-            <Button type="submit" color="#261FB3" fullWidth mt="xl">
+            <Button type="submit" color="#261FB3" fullWidth mt="xl" loading={loading}>
               Login
             </Button>
           </form>
