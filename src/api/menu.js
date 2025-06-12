42e6c0f2537ea2d930c240265d1b328b65504011
Vega Anggaresta <vegaangga98@gmail.com>
2025-06-12 16:08:06 +0700

    select get Jabatan

--- src/api/menu.js
+++ src/api/menu.js
@@ -1,3 +1,25 @@
 import { getToken } from "./auth";
 
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export async function fetchJabatan() {
+    const token = getToken();
+  const res = await fetch(`${BASE_URL}/getJabatan`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ codeJabatan: "" }),
+  });
+
+  const result = await res.json();
+
+  return result
+    .filter((item) => item.status === 1)
+    .map((item) => ({
+      value: item.code_jabatan,
+      label: `${item.code_jabatan} - ${item.namajabatan}`,
+    }));
+}
+
--- src/app/(main)/registrasi-user/page.js
+++ src/app/(main)/registrasi-user/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import {
   Stepper,
   Button,
@@ -10,42 +10,56 @@ import {
   Code,
   Card,
   Title,
-  Select
+  Select,
 } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import Breadcrumb from "@/components/BreadCrumb";
+import { fetchJabatan } from "@/api/menu";
+
 
 export default function RegistrasiUser() {
   const [active, setActive] = useState(0);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+  const getData = async () => {
+    try {
+      const result = await fetchJabatan();
+      setData(result);
+    } catch (err) {
+      console.error("Gagal ambil data jabatan:", err);
+    } finally {
+      setLoading(false);
+    }
+  };
+  getData();
+}, []);
 
   const form = useForm({
     mode: 'uncontrolled',
     initialValues: {
-        nippos: '',
-        email: '',
-        nama: '',
-        codeJabatan: '',
-        kantor: '',
-        statuspegawai: '',
-        statusakun: '',
-        password:''
+      nippos: '',
+      email: '',
+      nama: '',
+      codeJabatan: '',
+      kantor: '',
+      statuspegawai: '',
+      statusakun: '',
+      password: '',
     },
     validate: (values) => {
       if (active === 0) {
         return {
-          usernama:
-            values.nippos.trim().length < 6
-              ? 'Nippos'
-              : null,
-          password:
-            values.password.length < 6 ? 'Password must include at least 6 characters' : null,
+          nippos: values.nippos.trim().length < 6 ? 'Nippos minimal 6 karakter' : null,
+          password: values.password.length < 6 ? 'Password minimal 6 karakter' : null,
         };
       }
 
       if (active === 1) {
         return {
-          nama: values.nama.trim().length < 2 ? 'nama must include at least 2 characters' : null,
-          email: /^\S+@\S+$/.test(values.email) ? null : 'Invalid email',
+          nama: values.nama.trim().length < 2 ? 'Nama minimal 2 karakter' : null,
+          email: /^\S+@\S+$/.test(values.email) ? null : 'Format email tidak valid',
         };
       }
 
@@ -65,133 +79,124 @@ export default function RegistrasiUser() {
 
   return (
     <>
-      <Title order={3} mb="md">
-        Registrasi User
-      </Title>
-
+      <Title order={3} mb="md">Registrasi User</Title>
       <Breadcrumb />
+
       <Card shadow="md" padding="xl" radius="md" withBorder>
-      <Stepper active={active}>
-        <Stepper.Step label="First step" description="Data User">
-          <TextInput
-            label="Nippos"
-            placeholder="Nippos"
-            key={form.key('nippos')}
-            {...form.getInputProps('nippos')}
-            required
-          />
-          <TextInput
-            label="Email"
-            placeholder="Email"
-            key={form.key('email')}
-            {...form.getInputProps('email')}
-            required
-          />
-          <TextInput
-            label="Nama"
-            placeholder="Nama"
-            key={form.key('nama')}
-            {...form.getInputProps('nama')}
-            required
-          />
-          {/* Select Jabatan */}
-          <TextInput
-            label="NIPPOS"
-            placeholder="NIPPOS"
-            key={form.key('nippos')}
-            {...form.getInputProps('nippos')}
-            required
-          />
-          {/* Select Kantor */}
-          <TextInput
-            label="NIPPOS"
-            placeholder="NIPPOS"
-            key={form.key('nippos')}
-            {...form.getInputProps('nippos')}
-            required
-          />
-          <Select
-            label="Status Pegawai"
-            value={form.statuspegawai}
-             onChange={(val) => setForm({ ...form, status: val })}
-            data={[
-              { value: "1", label: "Aktif" },
-              { value: "0", label: "Tidak Aktif" },
-            ]}
-            placeholder="Pilih status"
-             required
-            mb="sm"
+        <Stepper active={active}>
+          <Stepper.Step label="First step" description="Data User">
+            <TextInput
+              label="Nippos"
+              placeholder="Nippos"
+              key={form.key('nippos')}
+              {...form.getInputProps('nippos')}
+              required
             />
+            <TextInput
+              label="Email"
+              placeholder="Email"
+              key={form.key('email')}
+              {...form.getInputProps('email')}
+              required
+            />
+            <TextInput
+              label="Nama"
+              placeholder="Nama"
+              key={form.key('nama')}
+              {...form.getInputProps('nama')}
+              required
+            />
+
+            {/* Select Jabatan */}
             <Select
-            label="Status Akun"
-            value={form.statusakun}
-             onChange={(val) => setForm({ ...form, status: val })}
-            data={[
-              { value: "1", label: "Aktif" },
-              { value: "0", label: "Tidak Aktif" },
-            ]}
-            placeholder="Pilih status"
-             required
-            mb="sm"
+              label="Pilih Jabatan"
+              placeholder={loading ? "Loading..." : "Pilih jabatan"}
+              data={data}
+              key={form.key('codeJabatan')}
+              {...form.getInputProps('codeJabatan')}
+              searchable
+              clearable
+              disabled={loading}
+              required
+              mt="md"
             />
-          <PasswordInput
-            mt="md"
-            label="Password"
-            placeholder="Password"
-            key={form.key('password')}
-            {...form.getInputProps('password')}
-          />
-        </Stepper.Step>
 
-        <Stepper.Step label="Second step" description="Personal information">
-          <TextInput
-            label="nama"
-            placeholder="nama"
-            key={form.key('nama')}
-            {...form.getInputProps('nama')}
-          />
-          <TextInput
-            mt="md"
-            label="Email"
-            placeholder="Email"
-            key={form.key('email')}
-            {...form.getInputProps('email')}
-          />
-        </Stepper.Step>
+            {/* Status Pegawai */}
+            <Select
+              label="Status Pegawai"
+              data={[
+                { value: "1", label: "Aktif" },
+                { value: "0", label: "Tidak Aktif" },
+              ]}
+              key={form.key('statuspegawai')}
+              {...form.getInputProps('statuspegawai')}
+              placeholder="Pilih status pegawai"
+              required
+              mt="md"
+            />
 
-        <Stepper.Step label="Final step" description="Social media">
-          <TextInput
-            label="Website"
-            placeholder="Website"
-            key={form.key('website')}
-            {...form.getInputProps('website')}
-          />
-          <TextInput
-            mt="md"
-            label="GitHub"
-            placeholder="GitHub"
-            key={form.key('github')}
-            {...form.getInputProps('github')}
-          />
-        </Stepper.Step>
+            {/* Status Akun */}
+            <Select
+              label="Status Akun"
+              data={[
+                { value: "1", label: "Aktif" },
+                { value: "0", label: "Tidak Aktif" },
+              ]}
+              key={form.key('statusakun')}
+              {...form.getInputProps('statusakun')}
+              placeholder="Pilih status akun"
+              required
+              mt="md"
+            />
 
-        <Stepper.Completed>
-          Completed! Form values:
-          <Code block mt="xl">
-            {JSON.stringify(form.getValues(), null, 2)}
-          </Code>
-        </Stepper.Completed>
-      </Stepper>
+            <PasswordInput
+              mt="md"
+              label="Password"
+              placeholder="Password"
+              key={form.key('password')}
+              {...form.getInputProps('password')}
+              required
+            />
+          </Stepper.Step>
 
-      <Group justify="flex-end" mt="xl">
-        {active !== 0 && (
-          <Button variant="default" onClick={prevStep}>
-            Back
-          </Button>
-        )}
-        {active !== 3 && <Button onClick={nextStep}>Next step</Button>}
-      </Group>
-    </Card>
+          <Stepper.Step label="Second step" description="Personal information">
+            <TextInput
+              label="Nama"
+              placeholder="Nama"
+              key={form.key('nama')}
+              {...form.getInputProps('nama')}
+            />
+            <TextInput
+              mt="md"
+              label="Email"
+              placeholder="Email"
+              key={form.key('email')}
+              {...form.getInputProps('email')}
+            />
+          </Stepper.Step>
+
+          <Stepper.Step label="Final step" description="Social media">
+            <TextInput label="Website" placeholder="Website" />
+            <TextInput mt="md" label="GitHub" placeholder="GitHub" />
+          </Stepper.Step>
+
+          <Stepper.Completed>
+            Completed! Form values:
+            <Code block mt="xl">
+              {JSON.stringify(form.getValues(), null, 2)}
+            </Code>
+          </Stepper.Completed>
+        </Stepper>
+
+        <Group justify="flex-end" mt="xl">
+          {active !== 0 && (
+            <Button variant="default" onClick={prevStep}>
+              Back
+            </Button>
+          )}
+          {active !== 3 && <Button onClick={nextStep}>Next step</Button>}
+        </Group>
+      </Card>
     </>
   );
 }
