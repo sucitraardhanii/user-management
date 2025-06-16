600a1298c4acd529044749be30952860bc754aaa
sucitraardhani <901495608@posindonesia.co.id>
2025-06-16 15:25:59 +0700

    notif login diatas

--- src/app/(auth)/layout.js
+++ src/app/(auth)/layout.js
@@ -1,13 +1,14 @@
-import '@mantine/core/styles.css';
-import { MantineProvider } from '@mantine/core';
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
 
 export default function AuthLayout({ children }) {
   return (
- <>
- <MantineProvider withGlobalStyles withNormalizeCSS>
-          {children}
-        </MantineProvider>
- </>
-        
+    <>
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <Notifications position="top-center" />
+        <>{children}</>
+      </MantineProvider>
+    </>
   );
 }
